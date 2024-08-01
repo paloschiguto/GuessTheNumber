3439490a80f706e25c705d798addcdef7c4f113a
Augusto Luis Paloschi <augusto.paloschi06@gmail.com>
2024-08-01 14:06:47 -0300

    Criada a validação para que o número informado seja menor que 100 e também, criada uma verificação para ver se o número informado já não foi ionformado anteriormente

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let computerNumber
 let userNumbers = []
-let attemps = 1
+let attemps = 0
 let maxGuesses = 10
 
 //gerando o numero sorteado
@@ -12,37 +12,43 @@ function init() {
 function newGame() {
     window.location.reload()
 }
-
 function compareNumbers() {
     const userNumber = Number(document.getElementById('inputBox').value)
-    userNumbers.push(' ' + userNumber)
-    document.getElementById('guesses').innerHTML = userNumbers
+    if (userNumber <= 100) {
+        if (!userNumbers.includes(' ' + userNumber)) {
+            userNumbers.push(' ' + userNumber)
+            document.getElementById('guesses').innerHTML = userNumbers
 
-    if (attemps < 10) {
-        if (userNumber > computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too high'
-            document.getElementById('inputBox').value = ''
-            attemps++
-            document.getElementById('attempts').innerHTML = attemps
-        } else if (userNumber < computerNumber) {
-            document.getElementById('textOutput').innerHTML = 'Your number is too low'
-            document.getElementById('inputBox').value = ''
-            attemps++
-            document.getElementById('attempts').innerHTML = attemps
-        } else {
-            if (attemps > 1) {
-                document.getElementById('textOutput').innerHTML = 'Congratulations! You got the number right in ' + attemps + ' attempts...'
-                document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+            if (attemps < 10) {
+                if (userNumber > computerNumber) {
+                    document.getElementById('textOutput').innerHTML = 'Your number is too high'
+                    document.getElementById('inputBox').value = ''
+                    attemps++
+                    document.getElementById('attempts').innerHTML = attemps
+                } else if (userNumber < computerNumber) {
+                    document.getElementById('textOutput').innerHTML = 'Your number is too low'
+                    document.getElementById('inputBox').value = ''
+                    attemps++
+                    document.getElementById('attempts').innerHTML = attemps
+                } else {
+                    if (attemps > 1) {
+                        document.getElementById('textOutput').innerHTML = 'Congratulations! You got the number right in ' + attemps + ' attempts...'
+                        document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+                    } else {
+                        document.getElementById('textOutput').innerHTML = 'Congratulations! You got the number right in ' + attemps + ' attempt...'
+                        document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+                    }
+                }
+
             } else {
-                document.getElementById('textOutput').innerHTML = 'Congratulations! You got the number right in ' + attemps + ' attempt...'
+                document.getElementById('textOutput').innerHTML = 'Game Over! You lost... The number was ' + computerNumber
                 document.getElementById('inputBox').setAttribute('readonly', 'readonly')
+                document.getElementById('inputBox').value = ''
             }
-
+        } else {
+            document.getElementById('textOutput').innerHTML = 'You already guessed this number'
         }
     } else {
-        document.getElementById('textOutput').innerHTML = 'Game Over! You lost... The number was ' + computerNumber
-        document.getElementById('inputBox').setAttribute('readonly', 'readonly')
-        document.getElementById('inputBox').value = ''
+        document.getElementById('textOutput').innerHTML = 'Please enter a number between 1 and 100'
     }
 }
-
